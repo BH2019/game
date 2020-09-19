199bb761c7a70eb72aff5b882a8f16f537013665
BH2019 <58276263+BH2019@users.noreply.github.com>
2020-09-18 18:10:20 -0700

    Add files via upload

---
+++ flappy_bird/main.js
@@ -0,0 +1,44 @@
+import Phaser from './lib/phaser.js'
+import GameScene from './scenes/GameScene.js'
+
+// This object contains all the Phaser configurations to load our game
+const config = {
+  	/**
+   	 * The type can be Phaser.CANVAS, Phaser.WEBGL or Phaser.AUTO. AUTO means that
+   	 * Phaser will try to render with WebGL, and fall back to Canvas if it fails
+   	 */
+	type: Phaser.AUTO,
+	backgroundColor:0x87ceeb,
+	scale: {
+		mode: Phaser.Scale.FIT,
+		autoCenter: Phaser.Scale.CENTER_BOTH,
+		width: 320,
+		height: 400
+	},
+	/**
+  	 * The physics engine determines how objects interact with the world. Phaser
+   	 * supports three physics engines out of the box: arcade, impact and matter.
+     * Arcade is understood to be the simplest one to implement
+   	*/
+
+	pixelArt: true,
+
+	physics: {
+		default: 'arcade',
+		arcade: {
+			gravity: {
+				y: 0
+			}
+		}
+	},
+	/**
+   	 * A scene is "self-contained" game world - all the logic and state of a game
+   	 * component. For e.g. it's common to a game menu to be one scene, whereas the
+     * first level is another scene. Phaser has a Scene object, but we can provide
+     * a regular JS object with these function names:
+     */
+	scene: [GameScene]
+}
+
+// Create the game instance
+export default new Phaser.Game(config)
---
+++ flappy_bird/scenes/GameScene.js
@@ -0,0 +1,147 @@
+import Phaser from '../lib/phaser.js'
+
+export default class GameScene extends Phaser.Scene
+{
+    constructor()
+    {
+        super('game-scene')
+
+        // Defining there variables here to let them can be accessed in all of the functions in this class
+        this.bird
+        this.pipeGroup
+
+        // Variable to determine if we started playing
+        this.gameStarted = false
+
+        this.rightMostPipeX = 100
+
+        this.pipePool = []
+
+        this.gameOptions = {
+
+            // horizontal bird speed
+            birdSpeed: 125,
+            
+            // bird gravity, will make bird fall if you don't flap
+            birdGravity: 800,
+
+            // flap thrust
+            birdFlapPower: 300,
+
+            // distance range from next pipe, in pixels
+            pipeDistance: [220, 280],
+
+            // hole range between pipes, in pixels
+            pipeHole: [100, 130],
+
+            // minimum pipe height, in pixels. Affects hole position
+            minPipeHeight: 50,
+        }
+    }
+
+    /**
+     * The function loads assets as Phaser begins to run the scene. The images are
+     * loaded as key value pairs, we reference the assets by their keys of course
+     */
+    preload()
+    {
+        this.load.image('pipe', 'assets/images/pipe.png');
+        this.load.image('bird', 'assets/images/bird.png');
+    }
+
+    /**
+     * We create our game world in this function. The initial state of our game is
+     * defined here. We also set up our physics rules here
+     */
+    create()
+    {
+        console.log(this.rightMostPipeX)
+        /**
+         * Coordinates start at 0,0 from the top left
+         * As we move rightward, the x value increases
+         * As we move downward, the y value increases.
+         */
+        this.bird = this.physics.add.sprite(
+            80,
+            this.physics.world.bounds.height / 2,
+            'bird' // key of image for the sprite
+        )
+        this.bird.body.gravity.y = this.gameOptions.birdGravity
+
+        this.pipeGroup = this.physics.add.group()
+        for(let i=0; i<4; i++)
+        {
+            this.pipePool.push(this.pipeGroup.create(0, 0, 'pipe'))
+            this.pipePool.push(this.pipeGroup.create(0, 0, 'pipe'))
+            this.placePipes()
+        }
+
+        this.pipeGroup.setVelocityX(-this.gameOptions.birdSpeed);
+
+        this.physics.add.collider(this.bird, this.pipeGroup, this.gameOver, null, this)
+
+        this.input.on('pointerdown', this.flap, this);
+    }
+
+    placePipes()
+    {
+
+        let pipeX = this.rightMostPipeX + this.pipePool[0].body.width + Phaser.Math.Between(this.gameOptions.pipeDistance[0], this.gameOptions.pipeDistance[1])
+
+        let pipeHoleHeight = Phaser.Math.Between(this.gameOptions.pipeHole[0], this.gameOptions.pipeHole[1]);
+        let pipeHolePosition = Phaser.Math.Between(this.gameOptions.minPipeHeight + pipeHoleHeight / 2, this.physics.world.bounds.height - this.gameOptions.minPipeHeight - pipeHoleHeight / 2);
+
+//            console.log(pipeHoleHeight, pipeHolePosition)
+
+        let pipeY1 = pipeHolePosition - pipeHoleHeight / 2
+        let pipeY2 = pipeHolePosition + pipeHoleHeight / 2
+
+        console.log(pipeX, pipeY1, pipeY2)
+
+        this.pipePool[0].x = pipeX
+        this.pipePool[0].y = pipeY1
+        this.pipePool[1].x = pipeX
+        this.pipePool[1].y = pipeY2
+
+        this.pipePool[0].setImmovable(true)
+        this.pipePool[1].setImmovable(true)
+
+        this.pipePool[0].setOrigin(0, 1)
+        this.pipePool[1].setOrigin(0, 0)
+
+        this.pipePool = []
+        this.rightMostPipeX = pipeX
+    }
+
+    update()
+    {
+        if (this.bird.y<0 || this.bird.y>this.physics.world.bounds.height)
+        {
+            this.gameOver()
+        }
+
+        this.pipeGroup.getChildren().forEach(function(pipe){
+            if(pipe.getBounds.right < 0)
+            {
+                this.pipePool.push(pipe)
+                if(this.pipePool.length == 2)
+                {
+                    this.placePipes()
+                }
+            }
+        })        
+    }
+
+    flap()
+    {
+        console.log("flap")
+        this.bird.setVelocityY(-this.gameOptions.birdFlapPower)
+    }
+
+    gameOver()
+    {
+        console.log("gameOver")
+        this.rightMostPipeX = 150
+        this.scene.start("game-scene")
+    }
+}
